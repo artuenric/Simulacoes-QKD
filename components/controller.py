e1ef0ae8b7764b781a9a1b0ed6c22ee60a99ab03
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2023-12-03 19:07:06 -0300

    Update Allocate Routes
    Agora as requisições tem prioridade. Valores de 1 a 5. As requests não necessariamente são atendidas na ordem baseada pela prioridade, deve-se levar em consideração também a política do FIFO.

--- components/controller.py
+++ components/controller.py
@@ -41,7 +41,7 @@ class Controller():
         """
         return sorted(list(nx.all_simple_paths(self.network.G, alice, bob)), key=len)
     
-    def calculate_k_shortest_routes(self, alice, bob, k=10):
+    def calculate_k_shortest_routes(self, alice, bob, k=5):
         """
         Procura k rotas de menor custo. Para o caso de redes do tipo malha, é mais efetivo que `calculate_shortest_routes()`, já que esta retorna apenas as rotas do canal direto alice-bob.
         Args:
@@ -65,19 +65,26 @@ class Controller():
     
     def allocate_routes(self, request_list, routes_calculation_type='shortest'):
         """
-        Aloca as rotas para os pedidos. E atualiza a lista de requisições.
+        Aloca as rotas para os pedidos e atualiza a lista de requisições.
         
         Args:
             request_list (list): Lista de requisições -> [alice, bob, app].
+        
+        Returns:
+            info (list): Lista de dicionários -> [{'Priority': priority, 'Route': route, 'App': app}].
         """
 
-        allocated_routes = []
+        # Informações das requisições
+        info = []
+        # Todos os links já utilizados
         all_used_links = set()
+        # Links utilizados apenas por BB84 e B92
         bb84_b92_used_links = set()
+        # Contador de rotas E91 (para que não haja muitos E91 com links compartilhados)
         e91_count = 0
-        list_app = []
-                
-        for alice, bob, app in request_list.copy():
+        
+                        
+        for alice, bob, app, priority in request_list.copy():
             # Calcula as rotas de menor custo
             if routes_calculation_type == 'shortest':
                 routes = self.calculate_shortest_routes(alice, bob)
@@ -85,10 +92,8 @@ class Controller():
                 routes = self.calculate_k_shortest_routes(alice, bob)
             elif routes_calculation_type == 'all':
                 routes = self.calculate_all_routes(alice, bob)
-            
-            print("Rotas calculada", routes)
+
             for route in routes:
-                print("Rota avaliada", route)
                 # Lista de pares de elementos adjacentes da lista route (canais)
                 route_links = [(route[i], route[i + 1]) for i in range(len(route) - 1)]
                 
@@ -98,15 +103,14 @@ class Controller():
                         if app == 'BB84' or app == 'B92':
                             bb84_b92_used_links.update(route_links)
                         
-                        # Adiciona a rota e o app na lista de rotas alocadas
-                        allocated_routes.append(route)
-                        list_app.append(app)
+                        # Adicionando as informações de rota, app e prioridade
+                        info.append({'Priority': priority, 'Route': route, 'App': app})
                         
                         # Adiciona os links usados no conjunto de links usados
                         all_used_links.update(route_links)
                         
                         # Remove a requisição da lista de requisições
-                        request_list.remove((alice, bob, app))
+                        request_list.remove((alice, bob, app, priority))
                         
                         break
                     
@@ -115,15 +119,19 @@ class Controller():
                     if not any(e91_link in bb84_b92_used_links for e91_link in route_links):
                         # Para que não haja tantos E91 com links compartilhados, só 3 rotas E91 que compartilham links serão alocadas no máximo
                         if e91_count < 3:
-                            allocated_routes.append(route)
-                            list_app.append(app)
+                            # Adicionando as informações de rota, app e prioridade
+                            info.append({'Priority': priority, 'Route': route, 'App': app})
+                            
+                            # Adicionando o link ao conjunto de links usados
                             all_used_links.update(route_links)
-                            request_list.remove((alice, bob, app))
+                            
+                            # Removendo a requisição da lista de requisições
+                            request_list.remove((alice, bob, app, priority))
                             e91_count += 1
                         
                         break
-                            
-        return allocated_routes, list_app
+        
+        return info
     
     def send_requests(self, request_list, routes_calculation_type='shortest'):
         """
@@ -133,36 +141,39 @@ class Controller():
             request_list (list): Lista de requisições -> [alice, bob, app].
         """
 
-        # 
+        # Índice da execução
         exec_index = 1
         results = dict()
         
         # Enquanto houver requisições na lista de requisições
         while len(request_list) > 0:
             # Alocando as rotas para os pedidos possíveis de serem atendidos, ou seja, que não compartilham links
-            allocated_routes, list_app = self.allocate_routes(request_list, routes_calculation_type)
+            requests_info = self.allocate_routes(request_list, routes_calculation_type)
                         
-            # Printando as informações da execução
+            #LOG
             #print(f'{exec_index}ª execução.')
             #print(f'Rotas alocadas: {allocated_routes}')
             #print(f'Apps: {list_app}')
             
-            for route, app in zip(allocated_routes, list_app):
-                # Alice é o primeiro elemento da rota e Bob é o último
-                alice, bob = route[0], route[-1]
-                qkd_app = app
+            requests_info = sorted(requests_info, key=lambda x: x['Priority'])
+            
+            for request in requests_info:
+                # Informações da requisição
+                alice, bob = request["Route"][0], request["Route"][-1]
+                app = request["App"]
+                route = request["Route"]
+                print(f"Prioridade {request['Priority']} - {app} - {alice} -> {bob}")
                 
                 # Executa a aplicação QKD
-                if qkd_app == 'B92':
+                if app == 'B92':
                     exec_data = run_qkd_b92(self.network, route)
-                elif qkd_app == 'BB84':
+                elif app == 'BB84':
                     exec_data = run_qkd_bb84(self.network, route)
-                elif qkd_app == 'E91':
+                elif app == 'E91':
                     exec_data = run_qkd_e91(self.network, route)
-
+            
             # Coletando dados
             results[exec_index] = exec_data
-            
             exec_index += 1
         
         return results
