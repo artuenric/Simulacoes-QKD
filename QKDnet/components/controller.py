ff221b81eb9f88d9b672299b8fb83b4133377f80
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2024-03-14 19:56:09 -0300

    Update controller.py
    Kshortest no lugar de shortest. Novo logger. time_left no lugar de current time para eliminação do request.

--- QKDnet/components/controller.py
+++ QKDnet/components/controller.py
@@ -1,13 +1,9 @@
 from ..protocols import *
+from ..utils import Logger
 from .finder import *
 from .data import DataBase
 
 import networkx as nx
-import logging as log
-log.basicConfig(
-    level=log.INFO,  # Nível mínimo de gravidade para registrar
-    format='%(asctime)s - %(message)s'  # Formato da mensagem de log
-)
 
 class Controller():
     """
@@ -18,7 +14,7 @@ class Controller():
         self.pathFinder = None
         self.sorter = None
         self.data_base = DataBase()
-        self.set_paths_calculation('shortest')
+        self.set_paths_calculation('kshortest')
         self.received_requests = []
         self.requests = []
         self.current_requests = []
@@ -94,33 +90,42 @@ class Controller():
         """
         
         # Itera sobre as requisições
-        for r in self.requests:
+        for r in self.requests.copy():
+            
+            # Se a requisição expirou, remove da lista de requisições
+            if r.time_left == 0:
+                Logger.get_instance().log(f"Request: {r.num_id} - Expirou!")
+                r.finished = True
+                self.data_base.collect_failed_requests_data(r)
+                self.requests.remove(r)
+                continue
+            
             # Calcula as rotas de menor custo para a request
             routes = self.pathFinder.get_paths(r.alice, r.bob)
             
-            log.info(f"Request: {r.num_id} - Rotas: {routes}")
+            Logger.get_instance().log(f"Request: {r.num_id} - Rotas: {routes}")
             
             # Itera sobre as rotas
             for route in routes:
                 # Lista de pares de elementos adjacentes da lista route (canais)
                 route_links = [(route[i], route[i + 1]) for i in range(len(route) - 1)]
                 
-                log.info(f"Capacidade da rota {route}: {list(self.network.channels[(link[0], link[1])]['capacity'] for link in route_links)}.")
-                log.info(f"Load da rota {route}: {list(self.network.channels[(link[0], link[1])]['load'] for link in route_links)}.")
+                Logger.get_instance().log(f"Capacidade da rota {route}: {list(self.network.channels[(link[0], link[1])]['capacity'] for link in route_links)}.")
+                Logger.get_instance().log(f"Load da rota {route}: {list(self.network.channels[(link[0], link[1])]['load'] for link in route_links)}.")
                 
                 # Se todos os links da rota tem capacidade maior que a carga atual, aloca a rota
                 if all(self.network.channels[(link[0], link[1])]["capacity"] > self.network.channels[(link[0], link[1])]["load"] for link in route_links):
                     r.route = route
-                    log.info(f"Request: {r.num_id} - Rota {route}.")
+                    Logger.get_instance().log(f"Request: {r.num_id} - Rota {route}.")
                     
                     self.current_requests.append(r)
-                    log.info(f"Request: {r.num_id} - Adicionado requests na lista de requests atuais.")
+                    Logger.get_instance().log(f"Request: {r.num_id} - Adicionado requests na lista de requests atuais.")
                     
                     self.network.add_load(route)
-                    log.info(f"Add Load na rota: {list(self.network.channels[(link[0], link[1])]["load"] for link in route_links)}")
+                    Logger.get_instance().log(f"Add Load na rota: {list(self.network.channels[(link[0], link[1])]["load"] for link in route_links)}")
                     break
         
-        log.info(f"Requests escolhidas para alocação: {list(request.num_id for request in self.current_requests)}")
+        Logger.get_instance().log(f"Requests escolhidas para alocação: {list(request.num_id for request in self.current_requests)}")
         
     
     def receive_requests(self, requests):
@@ -136,7 +141,7 @@ class Controller():
         self.requests.extend(requests)
         self.data_base.collect_all_requests_data(requests)
         
-        log.info(f"Requisições recebidas pelo Controlador: {list(request.get_info() for request in requests)}")
+        Logger.get_instance().log(f"Requisições recebidas pelo Controlador: {list(request.get_info() for request in requests)}")
 
         # Estima o tempo para atendimento dos requests
         self.estimate_time(self.requests)
@@ -144,7 +149,7 @@ class Controller():
         # Ordena as requisições
         self.requests = sorted(self.requests, key=lambda r: r.priority) #Talvez seja necessário implementar um método de ordenação próprio por meio de uma classe
 
-        log.info(f"Requisições ordenadas por tempo estimado: {list((request.num_id, request.estimated_time) for request in self.requests)}")
+        Logger.get_instance().log(f"Requisições ordenadas por tempo estimado: {list((request.num_id, request.estimated_time) for request in self.requests)}")
 
 
     def send_requests(self):
@@ -160,10 +165,10 @@ class Controller():
             # Aloca as rotas de acordo com o tempo de atendimento e atualiza a lista de requisições atuais.
             self.allocate()
 
-            log.info(f"Requests sendo atendidas: {list(request.num_id for request in self.current_requests)}")
+            Logger.get_instance().log(f"Requests sendo atendidas: {list(request.num_id for request in self.current_requests)}")
 
             for request in self.current_requests:
-                log.info(f"Request: {request.num_id} - Executando.")
+                Logger.get_instance().log(f"Request: {request.num_id} - Executando.")
 
                 # Executa a aplicação QKD
                 request.protocol.run(self.network, request.route)
@@ -173,22 +178,16 @@ class Controller():
                 # Coleta de dados
                 self.data_base.collect_protocol_data(request.protocol)
                 
-                log.info(f"Request: {request.num_id} - Chaves Obtidas: {len(request.protocol.shared_key)}")
-                log.info(f"Request: {request.num_id} - Chaves necessárias: {request.keys_need}")
+                Logger.get_instance().log(f"Request: {request.num_id} - Chaves Obtidas: {len(request.protocol.shared_key)}")
+                Logger.get_instance().log(f"Request: {request.num_id} - Chaves necessárias: {request.keys_need}")
 
                 if request.keys_need <= 0:
-                    log.info(f"Request: {request.num_id} - Atendida com sucesso.")
+                    Logger.get_instance().log(f"Request: {request.num_id} - Atendida com sucesso.")
                     request.served = True
                     self.data_base.collect_served_requests_data(request)
                     request.finished = True
                     self.requests.remove(request)
-                    log.info(f"Request: {request.num_id} - Removida da lista de requests.")
-
-                elif request.current_time == request.max_time:
-                    log.info(f"Request: {request.num_id} - Expirou!")
-                    request.finished = True
-                    self.data_base.collect_failed_requests_data(request)
-                    self.requests.remove(request)
+                    Logger.get_instance().log(f"Request: {request.num_id} - Removida da lista de requests.")
 
                 # "Limpa" a rota da requisição    
                 self.network.remove_load(request.route)
@@ -198,7 +197,7 @@ class Controller():
 
             # Atualiza o tempo de atendimento
             self.update_time()
-            log.info(f"Tempo atual: {self.time}")
+            Logger.get_instance().log(f"Tempo atual: {self.time}")
 
 
     def estimate_time(self, requests):
@@ -221,4 +220,4 @@ class Controller():
         """
         self.time += 1
         for request in self.requests:
-            request.current_time += 1
+            request.update_time()
