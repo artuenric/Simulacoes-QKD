962732b829e2827b923402d6134f644ddb3ad840
Arthur Henrique <111461334+artuenric@users.noreply.github.com>
2024-03-14 19:50:31 -0300

    Update kShortestPaths.py
    K como propriedade de PathFinder.

--- QKDnet/components/finder/kShortestPaths.py
+++ QKDnet/components/finder/kShortestPaths.py
@@ -6,8 +6,9 @@ class KShortestPaths(PathFinder):
         super().__init__()
         self.name = "K shortest paths"
         self.network = network
+        self.k = 5
         
-    def get_paths(self, alice, bob, k=5):
+    def get_paths(self, alice, bob):
         """
         Procura k rotas de menor custo. Para o caso de redes do tipo malha, é mais efetivo que `calculate_shortest_routes()`, já que esta retorna apenas as rotas do canal direto alice-bob.
         Args:
@@ -16,4 +17,4 @@ class KShortestPaths(PathFinder):
         Returns:
             route (list): Lista com k listas de nós que compõem a rota
         """
-        return list(islice(nx.shortest_simple_paths(self.network.G, alice, bob, weight=None),k ))
+        return list(islice(nx.shortest_simple_paths(self.network.G, alice, bob, weight=None), self.k ))
